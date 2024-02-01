15d07642aba538a266dbb4be3f4f019b6c9d0d5b
miakojason <miakojason@gmail.com>
2024-02-01 08:57:58 +0800

    edit+1

--- api-update-02/update0201-10.php
+++ api-update-02/update0201-10.php
@@ -2,7 +2,7 @@
 $table = $_POST['table'];
 $DB = ${ucfirst($table)};
 $row = $DB->find($_POST['id']);
-if ($_FILES['img']['tmp_name']) {
+if (isset($_FILES['img']['tmp_name'])) {
     move_uploaded_file($_FILES['img']['tmp_name'], "../img/" . $_FILES['img']['name']);
     $row['img'] = $_FILES['img']['name'];
 }
