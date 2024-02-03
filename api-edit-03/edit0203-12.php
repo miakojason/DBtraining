dc61873db8f0ac0339884e479d1f334a6a210877
miakojason <miakojason@gmail.com>
2024-02-03 23:59:56 +0800

    edit+1

--- api-edit-03/edit0203-12.php
+++ api-edit-03/edit0203-12.php
@@ -11,19 +11,20 @@ foreach ($_POST['id'] as $key => $id) {
         }
         switch ($table) {
             case "title":
-                $row['sh']=(isset($_POST['sh']) && $_POST['sh']==$id)?1:0;
+                $row['sh'] = (isset($_POST['sh']) && $_POST['sh'] == $id) ? 1 : 0;
                 break;
             case "admin":
-                $row['acc']=$_POST['acc'][$key];
-                $row['pw']=$_POST['pw'][$key];
+                $row['acc'] = $_POST['acc'][$key];
+                $row['pw'] = $_POST['pw'][$key];
                 break;
             case "menu":
-                $row['sh']=(isset($_POST['sh']) && in_array($id,$_POST['sh']))?1:0;
+                $row['href'] = $_POST['href'][$key];
+                $row['sh'] = (isset($_POST['sh']) && in_array($id, $_POST['sh'])) ? 1 : 0;
                 break;
-                default:
-                $row['sh']=(isset($_POST['sh']) && in_array($id,$_POST['sh']))?1:0;
+            default:
+                $row['sh'] = (isset($_POST['sh']) && in_array($id, $_POST['sh'])) ? 1 : 0;
         }
+        $DB->save($row);
     }
-    $DB->save($row);
 }
 to("../back.php?do=$table");
