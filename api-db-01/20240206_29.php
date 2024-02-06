80313ce1699c20762fdb3b177264c85adfa82c8e
miakojason <miakojason@gmail.com>
2024-02-06 23:57:44 +0800

    很想睡了但是睡前不管三七二十一就是給打完依次

--- api-db-01/20240206_29.php
+++ api-db-01/20240206_29.php
@@ -1,7 +1,121 @@
 <?php
 date_default_timezone_set("Asia/Taipei");
 session_start();
-class DB{
-    protected
+class DB
+{
+    protected $dsn = "mysql:host=loaclhost;charset=utf8;dbname=db08";
+    protected $pdo;
+    protected $table;
+    public function __construct($table)
+    {
+        $this->table = $table;
+        $this->pdo = new PDO($this->dsn, 'root', '');
+    }
+    function a2s($array)
+    {
+        foreach ($array as $col => $value) {
+            $tmp[] = "`$col`='$value'";
+        }
+        return $tmp;
+    }
+    function save($array)
+    {
+        if (isset($array['id'])) {
+            if (!empty($array)) {
+                
+            }
+        } else {
+            $sql = "insert into `$this->table `";
+            $cols = "(`" . join("`,`", array_keys($array)) . "`)";
+            $vals = "('" . join("','", $array) . "')";
+            $sql .= $cols . "values" . $vals;
+        }
+    }
+    function del($id)
+    {
+        $sql = "delete from `$this->table` where ";
+        if (is_array($id)) {
+            $tmp = $this->a2s($id);
+            $sql .= join(" && ", $tmp);
+        } elseif (is_numeric($id)) {
+            $sql .= "`id`='$id'";
+        } else {
+            echo "x type";
+        }
+        return $this->pdo->exec($sql);
+    }
+    function find($id)
+    {
+        $sql = "select * from `$this->table` where ";
+        if (is_array($id)) {
+            $tmp = $this->a2s($id);
+            $sql .= join(" && ", $tmp);
+        } elseif (is_numeric($id)) {
+            $sql .= "`id`='$id'";
+        } else {
+            echo "x type";
+        }
+        $row = $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+        return $row;
+    }
+    function sql_all($sql, $array, $other)
+    {
+        if (isset($this->table) && !empty($this->table)) {
+            if (is_array($array)) {
+                if (!empty($array)) {
+                    $tmp = $this->a2s($array);
+                    $sql .= " where " . join(" && ", $tmp);
+                }
+            } else {
+                $sql .= " $array ";
+            }
+            return $sql .= $other;
+        } else {
+            echo "x table";
+        }
+    }
+    function q($sql)
+    {
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    }
+    function all($where = '', $other = '')
+    {
+        $sql = "select * from `$this->table` ";
+        $sql = $this->sql_all($sql, $where, $other);
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    }
+    function count($where = '', $other = '')
+    {
+        $sql = "select count(*) from `$this->table` ";
+        $sql = $this->sql_all($sql, $where, $other);
+        return $this->pdo->query($sql)->fetchColumn();
+    }
+    function math($math, $col, $array = '', $other = '')
+    {
+        $sql = "select $math(`$col`) from `$this->table` ";
+        $sql = $this->sql_all($sql, $array, $other);
+        return $this->pdo->query($sql)->fetchColumn();
+    }
+    function sum($col = '', $where = '', $other = '')
+    {
+        return $this->math("sum", $col, $where, $other);
+    }
+    function max($col = '', $where = '', $other = '')
+    {
+        return $this->math("max", $col, $where, $other);
+    }
+    function min($col = '', $where = '', $other = '')
+    {
+        return $this->math("min", $col, $where, $other);
+    }
 }
-?>
+function dd($array)
+{
+    echo "<pre>";
+    print_r($array);
+    echo "</pre>";
+}
+function to($url)
+{
+    header("location:$url");
+}
