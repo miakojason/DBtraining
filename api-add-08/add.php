5e441f235194b7e5b8d6c3cd017b93ed3f2e7bf5
miakojason <miakojason@gmail.com>
2024-02-14 16:28:13 +0800

    add modal-upload

--- api-add-08/add.php
+++ api-add-08/add.php
@@ -6,7 +6,7 @@ switch ($table) {
         unset($_POST['pw2']);
         break;
 }
-if (isset($_FILES['img']['tmp_name'])) {
+if (!empty($_FILES['img']['tmp_name'])) {
     move_uploaded_file($_FILES['img']['tmp_name'], "../img/" . $_FILES['img']['name']);
     $_POST['img'] = $_FILES['img']['name'];
 }
