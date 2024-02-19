c8dab42cd9b8c4f2793dca5c6734da91b4913a07
miakojason <miakojason@gmail.com>
2024-02-19 16:35:05 +0800

    js reg一半沒打完

---
+++ 題組二/js/js0219-1.js
@@ -0,0 +1,14 @@
+function clean(){
+    $("input[type='text'],input[type='password'],input[type='number'],input[type='radio']").val("");
+}
+function reg(){
+    let user={
+        acc:$("#acc").val(),
+        pw:$("#pw").val(),
+        pw2:$("#pw2").val(),
+        email:$("#email").val(),
+    }
+    if(user.acc!='' && user.pw!='' && user.pw2!='' && user.emal!=''){
+        $.p
+    }
+}
